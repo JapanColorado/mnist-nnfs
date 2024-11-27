db925f21a215cc5ed5df10e1261d0f9703081dcb
JapanColorado <japancolorado@duck.com>
2024-11-26 21:52:02 -0700

    Add nice print statements and comments

--- scripts/network.py
+++ scripts/network.py
@@ -29,16 +29,17 @@ class Network:
     
     def SGD(self, training_data, epochs, mini_batch_size, eta,
             test_data=None):
-
-        n = len(training_data)
-
+        '''Train the network using minibatch stochastic gradient descent.'''
+        total_time = 0
         for j in range(epochs):
             time1 = time.time()
 
             random.shuffle(training_data)
+
+            # Split the data into groups with mini_batch_size datapoints in each
             mini_batches = [
                 training_data[k:k+mini_batch_size]
-                for k in range(0, n, mini_batch_size)]
+                for k in range(0, len(training_data), mini_batch_size)]
             
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
@@ -49,26 +50,33 @@ class Network:
                 print(f"Epoch {j}: {self.evaluate(test_data)} / {len(test_data)}, took {time2-time1:.2f} seconds")
             else:
                 print(f"Epoch {j} complete in {time2-time1:.2f} seconds")
+
+            total_time += time2-time1
+            
+        print(f"Final Accuracy: {self.evaluate(test_data) / len(test_data):.2f}")
+        print(f"Total Training Time: {total_time:.2f}")
     
     def update_mini_batch(self, mini_batch, eta):
+        '''Updates weights and biases using the gradients from one minibatch worth of data.'''
 
-        # Nabla_b and nabla_w are matrices that hold the partial derivatives
-        # of the cost function with respect to each bias(nabla_b) or weight(nabla_w)
+        # Empty weight and bias update matrices
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
 
         for x, y in mini_batch:
+            # Compute gradient with respect to w/b and then add to respective update matrices
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
 
+        # Take step of size eta opposite to gradient
         self.weights = [w - (eta * nw / len(mini_batch))
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta * nb / len(mini_batch))
                        for b, nb in zip(self.biases, nabla_b)]
         
     def backprop(self, x, y):
-        '''Use an index approach to backpropagation to compute the gradient of the cost function.'''
+        '''Compute the gradient of the cost function for the weights and biases for one data point.'''
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         
@@ -97,10 +105,16 @@ class Network:
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data):
+        '''Return the number of correct predictions of the network.'''
         test_results = [(np.argmax(self.feedforward(x)), y) 
                         for (x, y) in test_data]
         return sum(1 if x == y  else 0 for (x, y) in test_results)
 
-train, valid, test = load_data_wrapper()
-test_network = Network([784, 20, 20, 10])
-test_network.SGD(train, 30, 10, 3, test_data=test)
+if __name__ == "__main__":
+    train, valid, test = load_data_wrapper()
+    mnist_predictor = Network([784, 20, 20, 10])
+    mnist_predictor.SGD(train, 12, 10, 3, test_data=test)
+    
+    # Predict first 30 digits in validation set
+    for i in range(30):
+        print(f"Predicted: {np.argmax(mnist_predictor.feedforward(valid[i][0]))} Actual: {valid[i][1]}")
